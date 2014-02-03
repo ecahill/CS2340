6316e3b7366b37f7d65012d851f8355a8678b204
jwu308 <jwu308@gatech.edu>
2014-02-03 15:19:14 -0500

    Created Login Screen

---
+++ src/com/example/cs2340/LoginView.java
@@ -0,0 +1,15 @@
+package com.example.cs2340;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginView extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.login_view);
+	}
+
+}
