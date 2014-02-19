fb619d27673341875460b7cefe0ebc6ec030191a
Emily Cahill <c.emily@rocketmail.com>
2014-02-19 14:52:28 -0500

    updating old version

--- src/com/example/model/User.java
+++ src/com/example/model/User.java
@@ -1,6 +1,10 @@
 package com.example.model;
 
+
 public class User {
+//	int _id;
+//	String _username;
+//	String _password;
 	private String username;
 	private String password;
 	
@@ -8,6 +12,21 @@ public class User {
 		username = u;
 		password = p;
 	}
+	
+	/*public User(){}
+	
+	public User(int id, String u, String p){
+		this._id = id;
+		this._username = u;
+		this._password = p;
+	}
+	
+	public User(String u, String p){
+		this._username = u;
+		this._password = p;
+	}*/
+	
+	
 	
 	public String toString(){
 		return "Username: " + username;
